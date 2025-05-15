d539a999ab547ca5134566a260b33e6b1568aa94
Abhishek Airan <83405230+abhishekairan@users.noreply.github.com>
2025-05-15 23:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function fibonacci(num) {
-// your code here
-	if (n <= 1) {
-		return n;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+  if (num === 0) return 0;
+  if (num === 1) return 0;
+  if (num === 2) return 1;
+
+  let a = 0, b = 1;
+  for (let i = 3; i <= num; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
+}
+console.log(fibonacci(1)); 
+console.log(fibonacci(5)); 
+console.log(fibonacci(8)); 
 }
 
 module.exports = fibonacci;
